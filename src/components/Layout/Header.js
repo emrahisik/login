d32103d7309c96ae079450c83275ee9aa143fc8f
emrahisik <emrahisk@gmail.com>
2022-07-29 11:56:30 -0400

    login finished

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import logo from "../../gia-logo-wh.svg";
 
-
-export default function Header ({login, setLogin}){
+export default function Header({ login, setLogin }) {
   const clickHandler = () => {
-    setLogin(state=> !state)
-  }
+    setLogin((state) => !state);
+  };
   return (
     <header className={styles["app-header"]}>
-        <p>
-          GIA
-        </p>
-        <button className={styles.login} onClick={clickHandler}>Login</button>
-      </header>
-  )
+      <img src={logo} alt="logo" />
+      <button className={styles.login} onClick={clickHandler}>
+        Login
+      </button>
+    </header>
+  );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,45 +1,56 @@
 import React from "react";
-import styles from "./Login.module.css"
-import { Link } from "react-router-dom"
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
+import logo from "../../gia-logo-bl.svg"
 
-const Login = ({setLogin}) => {
-  const clickHandler = () => setLogin(state=>!state)
+const Login = ({ setLogin }) => {
+  const clickHandler = () => setLogin((state) => !state);
   return (
     <div>
-    <div className={styles.modal} onClick={clickHandler}></div>
-    <form className={`${styles["modal-content"]} ${styles.animate}`} action="action_page.php">
-      <div className={styles.container}>
-        <label htmlFor="uname">
-          <b>Username</b>
-        </label>
-        <input type="text" placeholder="Enter Username" name="uname" required />
+      <div className={styles.modal} onClick={clickHandler}></div>
+      <form
+        className={`${styles["modal-content"]} ${styles.animate}`}
+        action="action_page.php"
+      >
+        <div className={styles.imgcontainer}>
+          <img src={logo} alt="logo"/>
+        </div>
+        <div className={styles.container}>
+          <label htmlFor="user-id">User Id</label>
+          <input
+            type="text"
+            placeholder="Enter User Id"
+            name="user-id"
+            id="user-id"
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            id="password"
+            name="password"
+            required
+          />
+          <button type="submit">Login</button>
+          <label>
+            <input type="checkbox" name="remember" /> Remember me
+          </label>
+        </div>
 
-        <label htmlFor="psw">
-          <b>Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-        />
-
-        <button type="submit">Login</button>
-        <label>
-          <input type="checkbox" name="remember" /> Remember
-          me
-        </label>
-      </div>
-
-      <div className={styles.container} >
-        <button type="button" className={styles.cancelbtn} onClick={clickHandler}>
-          Cancel
-        </button>
-        <span className="psw">
-          Forgot <Link to="/">password?</Link>
-        </span>
-      </div>
-    </form>
+        <div className={styles.container}>
+          <button
+            type="button"
+            className={styles.cancelbtn}
+            onClick={clickHandler}
+          >
+            Cancel
+          </button>
+          <span className={styles.psw}>
+            Forgot <Link to="/">password?</Link>
+          </span>
+        </div>
+      </form>
     </div>
   );
 };
